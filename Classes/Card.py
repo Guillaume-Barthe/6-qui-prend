efdf235bab7ed478d3a15730218f65e16e33c556
Guillaume-Barthe <79014176+Guillaume-Barthe@users.noreply.github.com>
2021-06-11 14:04:54 +0200

    Update Card.py

--- Classes/Card.py
+++ Classes/Card.py
@@ -1,3 +1,4 @@
+
 class Card():
     '''
     A Card object contains two attributes, self.value describes the value
@@ -21,8 +22,10 @@ class Card():
         else:
             self.cost = 1
 
+    
+
+
     def __repr__(self):
 
         #return "This card has value "+str(self.value)+" and costs "+str(self.cost)+" points "
         return str(self.value)
-
